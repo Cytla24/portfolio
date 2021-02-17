7131bb1360be2ec3199209472e5a6260590c5284
aimie ojuba <ojuba2013@gmail.com>
2021-02-17 16:36:34 -0500

    changed news-template format

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -2,16 +2,21 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
+import Image from "gatsby-image"
 
 const ComponentName = ({ data }) => {
   const { content } = data.EachNews
+  const { image } = data.EachNews
+  const { title } = data.EachNews
 
   return (
     <Layout>
       <section className="blog-template">
         <div className="section-center">
           <article className="blog-content">
-            <ReactMarkdown source={content} />
+            <h1>{title}</h1>
+            <Image fluid={image.childImageSharp.fluid} />
+            <ReactMarkdown source={content} className="blog-text" />
             <Link to="/news" className="btn center-btn">
               news
             </Link>
@@ -26,6 +31,14 @@ export const query = graphql`
   query GetSingleNew($slug: String) {
     EachNews: strapiNews(slug: { eq: $slug }) {
       content
+      title
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   }
 `
